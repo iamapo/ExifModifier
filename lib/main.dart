ff68c27a0fd99f9084b36d94c5092ff9a3ef48cb
iamapo <redenius.andre@googlemail.com>
2024-11-19 23:47:23 +0100

    Layout change

--- lib/main.dart
+++ lib/main.dart
@@ -64,23 +64,25 @@ class _HomePageState extends State<HomePage> {
   Future<void> _showPermissionDialog() async {
     await showDialog(
       context: context,
-      builder: (BuildContext context) => AlertDialog(
-        title: Text(AppLocalizations.of(context)!.permissionDialogTitle),
-        content: Text(AppLocalizations.of(context)!.permissionDialogContent),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: Text(AppLocalizations.of(context)!.cancel),
+      builder: (BuildContext context) =>
+          AlertDialog(
+            title: Text(AppLocalizations.of(context)!.permissionDialogTitle),
+            content: Text(
+                AppLocalizations.of(context)!.permissionDialogContent),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text(AppLocalizations.of(context)!.cancel),
+              ),
+              TextButton(
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  await PhotoManager.openSetting();
+                },
+                child: Text(AppLocalizations.of(context)!.openSettings),
+              ),
+            ],
           ),
-          TextButton(
-            onPressed: () async {
-              Navigator.of(context).pop();
-              await PhotoManager.openSetting();
-            },
-            child: Text(AppLocalizations.of(context)!.openSettings),
-          ),
-        ],
-      ),
     );
   }
 
@@ -96,7 +98,8 @@ class _HomePageState extends State<HomePage> {
 
     List<AssetEntity> photosWithoutLocation = [];
     for (var album in albums) {
-      final List<AssetEntity> photos = await album.getAssetListPaged(page: 0, size: 50);
+      final List<AssetEntity> photos = await album.getAssetListPaged(
+          page: 0, size: 50);
       for (var photo in photos) {
         if (await _photoHasNoLocation(photo)) {
           photosWithoutLocation.add(photo);
@@ -126,7 +129,9 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(AppLocalizations.of(context)?.appTitle ?? 'Photos without Location'),
+        title: Text(AppLocalizations
+            .of(context)
+            ?.appTitle ?? 'Fotos ohne Standort'),
       ),
       body: _isLoading
           ? const Center(child: CircularProgressIndicator())
@@ -137,13 +142,15 @@ class _HomePageState extends State<HomePage> {
         itemBuilder: (context, index) {
           final photo = _photosWithoutLocation[index];
           return FutureBuilder<Uint8List?>(
-            future: photo.thumbnailDataWithSize(const ThumbnailSize(200, 200)),
+            future: photo.thumbnailDataWithSize(ThumbnailSize(MediaQuery
+                .of(context)
+                .size
+                .width
+                .toInt(), 300)),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.done &&
                   snapshot.hasData) {
-                return ListTile(
-                  leading: Image.memory(snapshot.data!),
-                  title: Text(photo.title ?? AppLocalizations.of(context)!.photoWithoutTitle),
+                return GestureDetector(
                   onTap: () {
                     Navigator.push(
                       context,
@@ -152,9 +159,31 @@ class _HomePageState extends State<HomePage> {
                       ),
                     );
                   },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.memory(
+                        snapshot.data!,
+                        width: double.infinity,
+                        height: 300,
+                        fit: BoxFit.cover,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          photo.title ??
+                              AppLocalizations.of(context)!.photoWithoutTitle,
+                          style: Theme
+                              .of(context)
+                              .textTheme
+                              .titleLarge,
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               }
-              return const CircularProgressIndicator();
+              return const Center(child: CircularProgressIndicator());
             },
           );
         },
